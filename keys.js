8615bb70c074ee35f0d6cf21ba8716e3320f8f15
Rich T <rtranter25@gmail.com>
2017-11-07 19:55:24 -0600

    adding more files, and keys, ready to start coding

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'stkJP09lr9o0yAUYJVDyW37WY',
+  consumer_secret: 'f6WluEBBVR43eCyMdnbD6JShrhVv8Bd38hMYAzrsL6jnQ4UPVI',
+  access_token_key: '927986316150738945-hjQ5Lv2oWjgF5BRoosu7dx8I1odh26l',
+  access_token_secret: 'Xy01cbUE3BKW36dVBQXU0JpfKQTEAqBNJzyv0qqcgu3t9',
+}
+
+module.exports = twitterKeys;
